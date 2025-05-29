7ad2b689c54ce3549348ecc5438bbc2dae0d3cc5
Mohamed <mouh4945@gmail.com>
2025-05-29 23:58:10 +0100

    update

--- Components/HomeTopNav.js
+++ Components/HomeTopNav.js
@@ -31,7 +31,7 @@ export default function HomeTopNav() {
             textTransform: "uppercase",
           }}
         >
-          youtube
+          youtube...
         </Text>
       </View>
     </View>
